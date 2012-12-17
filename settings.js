9ba7e3b3cd5e1b98abde4ef985a5aa86a73eb964
Evan Hazlett <ejhazlett@gmail.com>
2012-12-17 09:11:39 -0500

    fixed local_settings import

--- settings.js
+++ settings.js
@@ -4,16 +4,14 @@ try {
     var local = {};
 }
 
-var env = process.env.VCAP_SERVICES ? JSON.parse(process.env.VCAP_SERVICES) : {};
-var redis =  env.hasOwnProperty('redis-2.2') ? env['redis-2.2'][0]['credentials'] : {};
 exports.settings = {
     APP_TITLE: 'nginx-rt',
-    EXCLUDED_HOSTS: [],
-    PORT: 3000,
-    REDIS_HOST: redis.host || '127.0.0.1',
-    REDIS_PORT: redis.port || 6379,
-    REDIS_DB: redis.name || 0,
-    REDIS_PASS: redis.password || '',
-    SECRET_KEY: '1q2w3e4r5t6y7u8i9o0pAbCdOIUYT234fdrtjfjru7839d',
-    SESSION_SECRET: 'sessionkey'
+    EXCLUDED_HOSTS: local.EXCLUDED_HOSTS || [],
+    PORT: local.PORT || 3000,
+    REDIS_HOST: local.REDIS_HOST || '127.0.0.1',
+    REDIS_PORT: local.REDIS_PORT || 6379,
+    REDIS_DB: local.REDIS_DB || 0,
+    REDIS_PASS: local.REDIS_PASS || '',
+    SECRET_KEY: local.SECRET_KEY || '1q2w3e4r5t6y7u8i9o0pAbCdOIUYT234fdrtjfjru7839d',
+    SESSION_SECRET: local.SESSION_SECRET || 'sessionkey'
 }
